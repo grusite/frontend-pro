1e2376d32b8a31d28dc382730175691abf4f4fc3
Jorge Martín <grusite@gmail.com>
2019-09-24 00:16:20 +0200

    added css loader to fix import style in js

--- webpack.dev.js
+++ webpack.dev.js
@@ -7,4 +7,12 @@ module.exports = {
     path: path.join(__dirname, 'build'),
     filename: 'bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 }
