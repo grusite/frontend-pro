34cf39b980f065266bda771709be81779df74f19
Jorge Martín <grusite@gmail.com>
2019-09-24 00:15:44 +0200

    added css loader to be able to work with style.css import

--- webpack-examples/basic.js
+++ webpack-examples/basic.js
@@ -1,8 +1,9 @@
 // import suma from './test'
-import Cookie from 'js-cookie'
+// import Cookie from 'js-cookie'
 import moment from 'moment'
 import render from './render'
 import 'console-dot-frog'
+import 'style.css'
 
 console.log('Hello world!!')
 
@@ -11,7 +12,7 @@ const template = () => `
   ${moment().format('YYYY')}</div>
 `
 
-Cookie.set('value', 123)
+// Cookie.set('value', 123)
 
 console.frog('Awesome!!!')
 render(template(), document.body)
